7bcd94ce2e6a376f735a0d228a89ed0e603bbf6e
tiffanynuugen <tiffanynuugen@gmail.com>
2019-05-09 00:39:34 -0400

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,9 +8,22 @@ class SearchesController < ApplicationController
    @friends = foursquare.friends(session[:token])
   end
 
-  def coffee_shops
+  def foursquare
     foursquare = FoursquareService.new
-    @coffeeShops = foursquare.coffee_shops(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'])
+    @coffee_shops = foursquare.coffee_shops(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:zipcode])
+
+    body = JSON.parse(@coffee_shops.body)
+
+    if @coffee_shops.success?
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    render 'search'
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+      render 'search'
   end
 
 end
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -20,27 +20,14 @@ class FoursquareService
    JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 
-  def coffee_shops(client_id, client_secret)
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+  def coffee_shops(client_id, client_secret, zipcode)
+    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
+      req.params['near'] = zipcode
       req.params['query'] = 'coffee shop'
     end
-
-    body = JSON.parse(@resp.body)
-
-    if @resp.success?
-      @venues = body["response"]["venues"]
-    else
-      @error = body["meta"]["errorDetail"]
-    end
-    render 'search'
-
-    rescue Faraday::TimeoutError
-      @error = "There was a timeout. Please try again."
-      render 'search'
   end
 
 end
